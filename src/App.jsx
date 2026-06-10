4d93c80c03030fd33f6a6a0637f3ca3fddc3479c
sihabs-dev <mdsarks123@gmail.com>
2026-06-11 03:37:15 +0600

    add wining,restart,react-confetti feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from "./assets/hero.png";
 import "./App.css";
 import Button from "./components/button";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
   const [num, setNum] = useState(RandomNumber());
@@ -17,8 +18,13 @@ function App() {
     }));
   }
   function RandomNum() {
-    setNum(RandomNumber());
+    setNum((prev) =>
+      prev.map((item) =>
+        item.isHeld ? item : { ...item, value: Math.ceil(Math.random() * 6) },
+      ),
+    );
   }
+
   function hold(id) {
     setNum((prev) =>
       prev.map((item) =>
@@ -36,12 +42,34 @@ function App() {
       hold={hold}
     />
   ));
+  // let count = 0;
+  // let number = 0;
+  // num.map((item) => {
+  //   if (item.isHeld) {
+  //     count = count + 1;
+  //   }
+  //   if (item.value === num[1].value) {
+  //     number = number + 1;
+  //   }
+  // });
+  // count == num.length && num.length == number
+  // ? alert("congratulation you won the game!")
+  // : null;
+
+  let gameWon =
+    num.every((item) => item.isHeld) &&
+    num.every((item) => item.value === num[0].value);
+  console.log(gameWon);
+  function handleNewGame() {
+    setNum(RandomNumber());
+  }
   return (
     <div id="app-component">
+      {gameWon && <Confetti />}
       <h1>Play Tenzies Game</h1>
       <section id="btn-container">{RandomNumberButton}</section>
-      <button id="btn" onClick={RandomNum}>
-        Roll
+      <button id="btn" onClick={gameWon ? handleNewGame : RandomNum}>
+        {gameWon ? "New Game" : "Roll"}
       </button>
     </div>
   );
